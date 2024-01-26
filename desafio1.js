d7323428d0c9aa964a110260e741c48886b7b6d3
Jesus Gomez <gomjesal@gmail.com>
2024-01-26 20:14:37 -0300

    Crea el desafio1.js

---
+++ desafio1.js
@@ -0,0 +1,11 @@
+let diaDeSemana = prompt("Por favor indica que día de la semana es hoy");
+
+if (diaDeSemana == "Sábado") { 
+    alert("¡Buen fin de semana!"); 
+} else { 
+    if (diaDeSemana == "Domingo") { 
+        alert("¡Buen fin de semana!"); 
+    } else { 
+        alert("¡Buena semana!"); 
+    }
+}
