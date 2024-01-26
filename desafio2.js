d899fbcc8b4df34a35c5cf4ee4f6e18346f799f9
Jesus Gomez <gomjesal@gmail.com>
2024-01-26 20:20:32 -0300

    Crea el desafio2.js

---
+++ desafio2.js
@@ -0,0 +1,7 @@
+let numeroDelUsuario = prompt("Por favor ingresa un número");
+
+if (numeroDelUsuario >= 0) { 
+    alert("El número introducido es positivo");
+} else { 
+    alert("El número introducido es negativo"); 
+}
